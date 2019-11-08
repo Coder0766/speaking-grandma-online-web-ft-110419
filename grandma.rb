2759869e9a85a65fc6a9e8e562e08bae9800caed
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-08 01:32:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-    if 
+    if phrase == "I LOVE YOU GRANDMA!"
+       return "I LOVE YOU TOO PUMPKIN!"
+    
     
     
     if phrase == phrase.downcase
@@ -26,8 +28,7 @@ def speak_to_grandma(phrase)
   		return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "HUH?! SPEAK UP, SONNY!"
-    else phrase == "I LOVE YOU GRANDMA!"
-       return "I LOVE YOU TOO PUMPKIN!"
+    else 
     end
 end
 
