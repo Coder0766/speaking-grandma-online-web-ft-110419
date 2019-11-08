89d881863212574db76a8d830d4f5f51e2d0a978
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-08 01:29:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -28,7 +28,7 @@ def speak_to_grandma(phrase)
     end
 end
 
-speak_to_grandma("HI!")
+speak_to_grandma(phrase)
 
 describe '#speak_to_grandma' do
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
