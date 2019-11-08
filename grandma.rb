a929722046b815113835459cbe937a456f373e7b
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-08 01:56:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase)
      elsif phrase == phrase.downcase
   		 return "HUH?! SPEAK UP, SONNY!"
    
-     elsif phrase == |phrase.downcase|
+     elsif phrase == phrase.downcase
   		 return "HUH?! SPEAK UP, SONNY!"
     
      elsif phrase == phrase.upcase
