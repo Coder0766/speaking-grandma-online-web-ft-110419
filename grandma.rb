9fe8d08c7e2594372d7796e92a0e85170804e1b4
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-08 00:53:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,6 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     # If you shout, she can hear you (or at least she thinks so)
